52bd3c4beb9224a8c3992f0b635cdbcee33344c4
Bryce Handerson <bryce.handerson@gmail.com>
2013-02-19 12:56:51 -0800

    lab3 final + lab4 init

--- lab3/lab3/main.c
+++ lab3/lab3/main.c
@@ -4,24 +4,52 @@
 #include <unistd.h>
 #include <wait.h>
 #include <sys/types.h>
+#include <fcntl.h>
 
 char command[64], input[128], str[128];
 int i = 0;
+int filestyle=0;
+FILE *fp;
+char *iopos=NULL;
+
+void ioredir();
+void execute(char **myargv,char **envp);
 
 int main(int argc, char** argv, char **envp)
 {
-    /*
-    for(i=0; envp[i]!=0; i++)
-    {
-        printf("envp[%d]: %s\n", i,envp[i]);
-    }
-    */
     while(1)
     {
-        printf("\x1b[32m" "%s@MySh " "\x1b[34m" "%s $ " "\x1b[0m",envp[2]+5,get_current_dir_name());
+        printf("\x1b[32m" "%s@MySh " "\x1b[34m" "%s $ " "\x1b[0m",envp[2]+5,
+                get_current_dir_name());
         fgets(input,128,stdin);
         strcpy(str,input);
+        int oper;
+        //printf("strstr: %d\n",strstr(input,"<"));
 
+        if(memchr(input,'<',128)!=NULL)
+        {
+            filestyle=1;
+            iopos=memchr(input,'<',128);
+            iopos[strlen(iopos)-1] = '\0';
+
+        }
+        if(memchr(input,'>',128)!=NULL)
+        {
+            filestyle=2;
+            iopos=memchr(input,'>',128);
+            iopos++;
+            iopos[strlen(iopos)-1] = '\0';
+
+
+        }
+        if(strstr(input,">>")!=NULL)
+        {
+            filestyle=3;
+            iopos=strstr(input,">>");
+            iopos++;
+            iopos[strlen(iopos)-1] = '\0';
+        }
+        //printf("filestyle:%x\n",filestyle);
         char *token = strtok(str, " \n");
         if(token!=NULL)
         {
@@ -30,7 +58,7 @@ int main(int argc, char** argv, char **envp)
 
         if(strncmp(command, "exit",64)==0||strncmp(command, "q",64)==0)
         {
-            exit(1);
+            exit(EXIT_SUCCESS);
         }
 
         char args[64][64];
@@ -38,68 +66,83 @@ int main(int argc, char** argv, char **envp)
         i=0;
         while(token!=NULL)
         {
+            if(strcmp(token,"<")==0||strcmp(token,">")==0||strcmp(token,">>")==0)
+                break;
+            //printf("Token: %s\n",token);
             myargv[i] = token;
             i++;
             token = strtok(NULL, " \n");
         }
         myargv[i]=NULL;
+        execute(myargv,envp);
+    }// end while
 
-        for(i =0; myargv[i]!=0; i++)
-        {
-            printf("myargv[%d]: %s\n",i,myargv[i]);
-        }
-        i++;
-        printf("myargv[%d]: %s\n",i,myargv[i]);
+}//end main
 
-        if(strncmp(command, "cd",64)==0)
+void execute(char **myargv,char **envp)
+{
+    if(strncmp(command, "cd",64)==0)
+    {
+        if(myargv[1]==NULL)
         {
-            if(strncmp(myargv[1],"\0",64)==0)
-            {
-                chdir(envp[5]+5);
-                printf("CWD:%s\n",get_current_dir_name());
-            }
-            else
-            {
-                if(chdir(myargv[1])==-1)
-                    printf("chdir error\n");
-                printf("CWD:%s\n",get_current_dir_name());
-            }
+            chdir(envp[5]+5);
+            //printf("CWD:%s\n",get_current_dir_name());
         }
         else
         {
-            pid_t pid;
-            int status;
-            char *envp[] = { NULL };
-            char temp[64] = "/bin/";
-            myargv[0] = strcat(temp,command);
-            printf("myargv[0]=%s\n",myargv[0]);
+            if(chdir(myargv[1])==-1)
+                printf("chdir error\n");
+            printf("CWD:%s\n",get_current_dir_name());
+        }
+    }
+    char path[32][32] = {"/usr/local/sbin/ ","/usr/local/bin/",
+                                "/usr/sbin/","/usr/bin/","/sbin/",
+                                "/usr/games/","/usr/local/games","/bin/"};
+    pid_t pid;
+    int status=1;
+    //char *envp[] = { NULL };
+    for(i=0;i<8&&status!=0;i++)
+    {
+        myargv[0] = strcat(path[i],command);
 
-            switch (pid = fork())
+        switch (pid = fork())
+        {
+        case -1:
+            perror("fork()");
+            exit(EXIT_FAILURE);
+        case 0:
+            if(filestyle==1)
             {
-            case -1:
-                perror("fork()");
-                exit(EXIT_FAILURE);
-            case 0:
-                status = execve(myargv[0],myargv,envp);
-                //break;
-                exit(status);
-            default:
-                if( waitpid(pid, &status, 0) < 0)
-                {
-                    perror("waitpid()");
-                    exit(EXIT_FAILURE);
-                }
-                if( WIFEXITED(status))
-                {
-                    break; // do not exit get next input
-                    //exit(WEXITSTATUS(status));
-                }
+                filestyle=0;
+            } else
+            if(filestyle==2)
+            {
+                close(1);
+                open(iopos+1, O_WRONLY|O_CREAT, 0644);
+                filestyle=0;
+            }
+            if(filestyle==3)
+            {
+                close(1);
+                printf("IOPOS: %s",iopos);
+                open(iopos+2, O_RDWR|O_APPEND, 0644);
+                filestyle=0;
+            }
+            status = execve(myargv[0],myargv,envp);
+            exit(status);
+        default:
+            if( waitpid(pid, &status, 0) < 0)
+            {
+                perror("waitpid()");
                 exit(EXIT_FAILURE);
             }
-
-
-        }// end else
-
+            if( WIFEXITED(status))
+            {
+                if(status != 65280)
+                    printf("child exit status code: %d\n",status);
+                break; // do not exit get next input
+            }
+            exit(EXIT_FAILURE); // should not ever get here
+        }
     }
-    return 0;
 }
