d9d44efbf627af37c1d5a3f97335ea091d2f14df
Bryce Handerson <bryce.handerson@gmail.com>
2013-02-22 09:07:44 -0800

    cleaned up some files

--- lab4/lab4/main.c
+++ lab4/lab4/main.c
@@ -5,7 +5,7 @@
 
 void printstat(struct stat s);
 void getinput();
-void clearstat(struct stat s);
+//void clearstat(struct stat s);
 
 char input[128];
 
@@ -14,41 +14,42 @@ int main()
     struct stat s;
     char *inputPtr=input;
     getinput(inputPtr);
+    //check if exiting or invalid file
     while(strcmp(inputPtr,"exit")!=0 && lstat(input,&s)==0)
     {
-
-        if(S_ISLNK(s.st_mode)) // if s is a link print filename -> linkname
+        if(S_ISLNK(s.st_mode))   // if s is a link print filename -> linkname
         {
-            char *filename;
+            char *filename;     // input filename
             filename = strrchr(input,'/');
-            char name[64];
-            char *namePtr;
+            char name[64] = {0};
+            char *namePtr;      // linked filename
             readlink(input, name, 64);
             namePtr = strrchr(name,'/');
             stat(input,&s);
             printstat(s);
-            printf(" %s -> %s\n",filename+1,namePtr+1);
+            printf(" %s -> %s\n",filename+1,namePtr+1);// +1 gets rid of /
             printf("\n");
 
         }
-        else if(S_ISDIR(s.st_mode)) // if s is a dir
+        else if(S_ISDIR(s.st_mode))     // if s is a dir
         {
-            DIR *dp = opendir(input);
-            struct dirent *ep = readdir(dp);
-            printf("File: %s\n",input);
+            DIR *dp = opendir(input);   // open the directory
+            struct dirent *ep = readdir(dp);    // browse to first file
+            // print header
             printf("File_type\tPermissions\tUid\tSize\tCreation Time\n"
                    "---------\t-----------\t---\t----\t-------------\n");
+            // while there are more files in directory
             while((ep = readdir(dp))!=0)
             {
                 char c[128];
                 strncat(c,input,64);
                 strcat(c,"/");
-                strncat(c,ep->d_name,64);
+                strncat(c,ep->d_name,64); // create path
                 lstat(c,&s);
-				printstat(s);
-				printf(" %s",ep->d_name);
-				printf("\n");
-				printf("C: %s \n",c);
+                printstat(s);
+                printf(" %s",ep->d_name);
+                printf("\n");
+                //printf("C: %s \n",c);
                 //clearstat(s);
                 c[0]='\0';
 
@@ -102,29 +103,12 @@ void printstat(struct stat s)
     printf("%s",i);
 }
 
-void getinput()
+void getinput()   // get an input from the user type: /home/bryce/...
 {
     char in[128];
     printf("input path: ");
     gets(in);
     strcpy(input,in);
 }
-
-
-void clearstat(struct stat s)
-{
-    s.st_blksize = 0;
-    s.st_blocks = 0;
-    s.st_dev = 0;
-    s.st_gid = 0;
-    s.st_ino = 0;
-    s.st_mode = 0;
-    s.st_nlink = 0;
-    s.st_rdev = 0;
-    s.st_size = 0;
-    s.st_uid = 0;
-}
-
-
 
 
